3b0ba5f9106ddc5c8819cb7af7ac114e1abef762
BiliGoldenWater <2439577268@qq.com>
2022-12-03 17:28:30 +0800

    Initial commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,13 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+fn main() {
+    tauri::Builder::default()
+        .register_uri_scheme_protocol("testUriScheme", |_app_handle, _req| {
+            tauri::http::ResponseBuilder::new().body("test_body".to_string().as_bytes().to_vec())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}

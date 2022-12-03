d76061ba0b7f948b1dad5f0486a6ca2b435c1d0c
BiliGoldenWater <2439577268@qq.com>
2022-12-03 17:35:48 +0800

    add async example

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,13 @@
 #![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
+all(not(debug_assertions), target_os = "windows"),
+windows_subsystem = "windows"
 )]
 
 fn main() {
-    tauri::Builder::default()
-        .register_uri_scheme_protocol("testUriScheme", |_app_handle, _req| {
-            tauri::http::ResponseBuilder::new().body("test_body".to_string().as_bytes().to_vec())
-        })
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  tauri::Builder::default()
+    .register_uri_scheme_protocol("testUriScheme", |_app_handle, _req| {
+      tauri::http::ResponseBuilder::new().header("Access-Control-Allow-Origin", "*").body("test_body".to_string().as_bytes().to_vec())
+    })
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }

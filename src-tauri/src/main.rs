06a7b078e9ddfcef6b7156d9d3cecfc317d76d1f
BiliGoldenWater <2439577268@qq.com>
2022-12-03 18:00:25 +0800

    add show hex option

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,7 +6,9 @@ windows_subsystem = "windows"
 fn main() {
   tauri::Builder::default()
     .register_uri_scheme_protocol("testUriScheme", |_app_handle, _req| {
-      tauri::http::ResponseBuilder::new().header("Access-Control-Allow-Origin", "*").body("test_body".to_string().as_bytes().to_vec())
+      tauri::http::ResponseBuilder::new()
+        .header("Access-Control-Allow-Origin", "*")
+        .body("test_long_body".to_string().as_bytes().to_vec())
     })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
